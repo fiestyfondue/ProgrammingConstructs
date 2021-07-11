e6b55efa154febc4e6fa4faf8646ba63913bee3b
Harsh <harsh.21483@gmail.com>
2021-07-11 03:51:59 -0700

    [Harsh]Add:- Taking User input as digit and converting it to Word

---
+++ NumberToWord.js
@@ -0,0 +1,44 @@
+let input = require('readline-sync');  //Function to get input from User
+let digit=input.question("Enter a Single Digit:");   //Similaar to Console.readline()
+digit=Number(digit); //Converting digit to int
+
+if(digit == 0)
+{
+    console.log("Zero");
+}
+else if(digit == 1)
+{
+    console.log("One");
+}
+else if(digit == 2)
+{
+    console.log("Two");
+}
+else if(digit == 3)
+{
+    console.log("Three");
+}
+else if(digit == 4)
+{
+    console.log("Four");
+}
+else if(digit == 5)
+{
+    console.log("Five");
+}
+else if(digit == 6)
+{
+    console.log("Six");
+}
+else if(digit == 7)
+{
+    console.log("Seven");
+}
+else if(digit == 8)
+{
+    console.log("Eight");
+}
+else if(digit == 9)
+{
+    console.log("Nine");
+}
